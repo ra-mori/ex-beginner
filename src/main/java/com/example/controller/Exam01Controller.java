b76f09ca182330ca39de7cdedec611a4460b5a9c
ra-mori <shigeki.morishita@rakus.co.jp>
2021-05-20 16:13:37 +0900

    間違い個所の修正をしました

--- src/main/java/com/example/controller/Exam01Controller.java
+++ src/main/java/com/example/controller/Exam01Controller.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,5 +10,12 @@ public class Exam01Controller {
 	@RequestMapping("")
 	public String index() {
 		return "exam01";
+	}
+
+	@RequestMapping("/result")
+	public String result(String name, Model model) {
+		model.addAttribute("name", name);
+
+		return "exam01-result";
 	}
 }
