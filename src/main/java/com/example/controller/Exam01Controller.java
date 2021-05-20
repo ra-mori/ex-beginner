9b4d266c87b3df6fefe9f8d4e55f7a38200c915b
ra-mori <shigeki.morishita@rakus.co.jp>
2021-05-20 15:16:20 +0900

    名前の送信機能を実装しました。

---
+++ src/main/java/com/example/controller/Exam01Controller.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/exam01")
+public class Exam01Controller {
+	@RequestMapping("")
+	public String index() {
+		return "exam01";
+	}
+}
