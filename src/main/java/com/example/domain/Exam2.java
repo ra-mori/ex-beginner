21f4824ef20d5010d055c701d2fa4b403a9ad06d
ra-mori <shigeki.morishita@rakus.co.jp>
2021-05-20 16:51:08 +0900

    トップページを作成

---
+++ src/main/java/com/example/domain/Exam2.java
@@ -0,0 +1,28 @@
+package com.example.domain;
+
+public class Exam2 {
+	private Integer sum1;
+	private Integer sum2;
+
+	public Integer getSum1() {
+		return sum1;
+	}
+
+	public void setSum1(Integer sum1) {
+		this.sum1 = sum1;
+	}
+
+	public Integer getSum2() {
+		return sum2;
+	}
+
+	public void setSum2(Integer sum2) {
+		this.sum2 = sum2;
+	}
+
+	@Override
+	public String toString() {
+		return "Exam2 [sum1=" + sum1 + ", sum2=" + sum2 + "]";
+	}
+
+}
